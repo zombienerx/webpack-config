5adfafb3f9af62f12c457656b50cfb01822faba9
zombienerx <nermin.sehic@gmail.com>
2021-10-09 11:44:14 +0200

    add babel to project

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ const nerkoRecipe =  {
   letherStripe: 2,
   ironIngot: 1,
   refineMoonstone: 4
-}
+};
+
+const nerkoletsRecipe = {
+  ...nerkoRecipe,
+  leather: 1,
+  refineMoonstone: 4,
+};
 
 console.log(nerkoRecipe);
+console.log(nerkoletsRecipe);
