debc39b0a6abd7ca76664c882387708335d25069
zombienerx <nermin.sehic@gmail.com>
2021-10-10 19:04:47 +0200

    add image support to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -11,12 +11,23 @@ module.exports = {
   mode: mode,
   target: target,
 
+  output: {
+    assetModuleFilename: "images/[hash][ext][query]",
+  },
+
   module: {
     rules: [
       {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: "asset/resource",
+      },
+      {
         test: /\.(s[ac]|c)ss$/i,
         use: [
-          MiniCssExtractPlugin.loader,
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: { publicPath: ""},
+          },
           "css-loader",
           "postcss-loader",
           "sass-loader"],
